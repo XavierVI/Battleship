540cfffdf47a60018404300cbdcd8dc19edb4fcd
Xavier Barr <vigenesisvi@gmail.com>
2023-05-02 19:53:58 -0600

    refactored game.test.js and Game.js

--- src/Game.js
+++ src/Game.js
@@ -2,7 +2,7 @@ import {zeros} from 'mathjs';
 
 class Game{
     constructor(){
-        this.player1 = getShips();
+        
     }
 }
 
@@ -10,4 +10,59 @@ function player(){
 
 }
 
-export {Game}
+// handles establishing the starting point of a ship
+// returns coordinate as an array [x, y]
+function getCoords(shipSize, takenCoords){
+    // 50% chance of horizontal and vertical orientation
+    const horizontal = Math.floor(Math.random*2);
+    let shipCoord = [];
+    let startingCoord;
+
+    do{
+        // generate new coordinate until while loop condition fails
+        const x = Math.floor(Math.random()*shipSize);
+        const y = Math.floor(Math.random()*shipSize);
+        startingCoord = [x, y];
+
+        // check if current starting location is available and orientation is horizontal
+        if(!contains(takenCoords, startingCoord) && horizontal){
+            shipCoord.push(startingCoord);
+            // loop through each neighbor cell to check availablity
+            for(let i = 1; i < shipSize; i++){
+                if(!contains(takenCoords, [x+i, y])) shipCoord.push([x+i, y]);
+                // if neighbor cell is taken, start over
+                else{
+                    // reset shipCoord because this coord does not work
+                    shipCoord = [];
+                    break;
+                }
+            }
+        }
+        // same condition, this block is for vertical orientations
+        else if(!contains(takenCoords, startingCoord)){
+            shipCoord.push(startingCoord);
+            // loop through each neighbor cell to check availablity
+            for(let i = 1; i < shipSize; i++){
+                if(!contains(takenCoords, [x, y+i])) shipCoord.push([x, y+i]);
+                // if neighbor cell is taken, start over
+                else{
+                    // reset shipCoord because this coord does not work
+                    shipCoord = [];
+                    break;
+                }
+            }
+        }
+    } while(shipCoord.length === 0)
+    // loop ends when shipCoord length is not zero
+    return shipCoord;
+}
+
+function contains(arr, coord){
+    for(let i = 0; i < arr.length; i++){
+        // convert array values to strings for simple comparison
+        if(arr[i].toString() === coord.toString()) return true;
+    }
+    return false;
+}
+
+export {Game, player, getCoords}
--- tests/game.test.js
+++ tests/game.test.js
@@ -1,26 +1,41 @@
-import {Game} from '../src/Game.js';
+import {Game, player, getCoords} from '../src/Game.js';
 import {typeOf, zeros} from 'mathjs';
 
-const gameBoard = document.getElementById('game-board');
+// const gameBoard = document.getElementById('game-board');
 const game = new Game();
-const player = player();
+const playerTest = player();
 
-describe('Testing generated ships for players', () => {
-    test('getCoords test', () => {
-        // getCoords returns appropriate bounds for a ship
-        expect(   getCoords(5) < 6
-               && getCoords(4) < 7
-               && getCoords(3) < 8
-               && getCoords(2) < 9  )
-                .toBe(true);
+describe('Generating player ships and coords', () => {
+    test('getCoords array size', () => {
+        const c1 = getCoords(2, []);
+        const c2 = getCoords(3, []);
+        const c3 = getCoords(4, []);
+        const c4 = getCoords(5, []);
+        // expect getCoords to return an array with correct size
+        expect( c1.length === 2 
+             && c2.length === 3 
+             && c3.length === 4 
+             && c4.length === 5)
+             .toBe(true);
     });
+
+    test('getCoords returning available coordinates', () => {
+        const arr = [[1, 4], [6, 6], [7, 8], [1, 1], [4, 8]];
+        const size = 4;
+        const coords = getCoords(size, arr);
+        // coords should not have values from arr
+        expect(arr).not.toContain(coords);
+    });
+});
+
+describe('player function', () => {
     // player contains all ships
-    test('ship population', () => {
-        expect(   player.carrier
-               && player.battleship
-               && player.crusier
-               && player.submarine
-               && player.destroyer  )
+    test('ship population and sizes', () => {
+        expect(   playerTest.carrier
+               && playerTest.battleship
+               && playerTest.crusier
+               && playerTest.submarine
+               && playerTest.destroyer  )
                 .toBe(true);
     });
 });
@@ -33,7 +48,7 @@ describe('DOM element tests', () => {
     });
 
     test('Action listeners test', () => {
-        addShipListener(player);
+        addShipListener(playerTest);
         expect(gameBoard.childNodes)
     });
 });
